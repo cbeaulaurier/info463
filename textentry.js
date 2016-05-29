450d9e313e12a8c4647c4f95ac5d77acc17d7af4
Cassandra Beaulaurier <cbeaulaurier@Cassandras-MacBook-Pro.local>
2016-05-29 16:16:48 -0700

    added gesture and click functionality

--- textentry.js
+++ textentry.js
@@ -6,49 +6,72 @@
 var startX,
     startY,
     endX,
-    endY;
+    endY, 
+    relX,
+    relY;
 
 var letters = [[null, "b", "c", "a"], [null, "d", "f", "e"], ["i", "g", "h", null],
                 [null, "j", "k", "l"], [null, "m", "o", "n"], ["r", "p", "q", null],
                 [null, "s", "u", "t"], [null, "v", "x", "w"], [" ", "y", "z", null]];
 
+// var letters = [[null, "b", "c", "a"], [null, "j", "k", "l"], [null, "s", "u", "t"], 
+//                 [null, "d", "f", "e"], [null, "m", "o", "n"], [null, "v", "x", "w"],
+//                 ["i", "g", "h", null], ["r", "p", "q", null], [" ", "y", "z", null]];
+
 // records x and y coordinates of mousedown
 function recordStart(event) {
     startX = event.pageX;
     startY = event.pageY;
-
-    console.log("x = " + startX + ", y = " + startY);
+    getRelativePosition()
 }
 
 // records x and y coordinates of mouseup and starts calculation process
 function recordEnd(event) {
     endX = event.pageX;
     endY = event.pageY;
-    console.log("x = " + endX + ", y = " + endY);
 
     matchAngle();
+}
+
+// gets mouse coordinates relative to canvas
+function getRelativePosition() {
+    var rect = document.getElementById("myCanvas").getBoundingClientRect();
+    relX = startX - rect.left;
+    relY = startY - rect.top;
 }
 
 // 
 function matchAngle() {
 
+    var box = getBox();
     if (startX == endX && startY == endY) {
         // TREAT AS CLICK
-        console.log("click");
+        if (box == 2 || box == 5 || box == 8) {
+            console.log(letters[box][0]);
+        } else {
+            console.log(letters[box][3]);
+        }
     } else {
         // TREAT AS GESTURE
         var angle = computeAngle();
-        console.log(angle);
-        // TODO: unit circle if statements
+
+        // compare angle to unit circle
         if (angle >= -135 && angle < -45) {
-            console.log("upward");
-            console.log(letters[0][1]);
+            if (letters[box][1]) {
+                console.log(letters[box][1]);
+            }
         } else if (angle >=-45 && angle < 45) {
-            console.log("rightward");
+            if (letters[box][3]) {
+                console.log(letters[box][3]);
+            }
         } else if (angle >= 45 && angle < 135) {
-            console.log("downward");
+            if (letters[box][2]) {
+                console.log(letters[box][2]);
+            }
         } else {
-            console.log("leftward");
+            if (letters[box][0]) {
+                console.log(letters[box][0]);
+            }
         }
     }
 
@@ -69,4 +92,34 @@ function computeAngle() {
     }
     var degrees = radians * (180/Math.PI);
     return degrees;
+}
+
+// returns the number of the box that the gesture begins in
+// note: code is kind of backwards but the box numbers are correct
+function getBox() {
+    if (relX <= 116) {  // column 1
+        if (relY <= 133) { // row 1
+            return 0;
+        } else if (relY > 133 && relY <= 266) { // row 2
+            return 3;
+        } else {    // row 3
+            return 6;
+        }
+    } else if (relX > 116 && relX <= 224) {  // column 2
+        if (relY <= 133) { // row 1
+            return 1;
+        } else if (relY > 133 && relY <= 266) { // row 2
+            return 4;
+        } else {    // row 3
+            return 7;
+        }
+    } else {  // column 3
+        if (relY <= 133) { // row 1
+            return 2;
+        } else if (relY > 133 && relY <= 266) { // row 2
+            return 5;
+        } else {    // row 3
+            return 8;
+        }
+    }
 }
